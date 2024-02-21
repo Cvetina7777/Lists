90e9470c9106ab23dfdefedb90caf2c3e3dad3db
Cvetina7777 <tt70207252@edu1.pgzaimov.com>
2024-02-21 08:31:36 +0200

    dhjk

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ListCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedete spisuk ot cvetove > Done za krai");
+            List<string> colorlist = new List<string>();
+            string Input = Console.ReadLine();
+            while(Input != "Done")
+            {
+              colorlist.Add(Input);
+                Input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Koi element da iztriesh 0-" + (colorlist.Count-1));
+            int index=int.Parse(Console.ReadLine());
+
+            colorlist.RemoveAt(index);
+
+            colorlist[0] = "Pink";
+
+
+            //for(int i = 0; i < colorlist.Count; i++)
+            // {
+            //     Console.WriteLine(colorlist[1]);
+            // }
+            foreach (string chervenokvadratche in colorlist)
+            {
+                Console.WriteLine(chervenokvadratche);
+            }
+        }
+    }
+}
